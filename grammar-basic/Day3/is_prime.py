d173cf992f44be261274b7cf9875d57239803f83
zhent1106 <1299088269@qq.com>
2020-03-27 15:12:36 +0800

    Day3

---
+++ grammar-basic/Day3/is_prime.py
@@ -0,0 +1,17 @@
+"""
+输入一个正整数判断是否为素数
+"""
+
+from math import sqrt
+
+num = int(input('请输入一个正整数：'))
+end = int(sqrt(num))
+is_prime = True
+for x in range(2, end+1):
+    if num % x == 0:
+        is_prime = False
+        break
+if is_prime and num != 1:
+    print('%d是素数' % num)
+else:
+    print('%d不是素数' % num)
