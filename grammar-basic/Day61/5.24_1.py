4e68e23b7fa09ed8f691887824f3955c4af97cee
zhent1106 <1299088269@qq.com>
2020-05-25 21:15:38 +0800

    Day62

--- grammar-basic/Day61/5.24_1.py
+++ grammar-basic/Day61/5.24_1.py
@@ -10,7 +10,7 @@ print(a)
 # 返回一个元组，带有2个np array对象，分别表示大于3的元素第一维，第二维度中的位置
 b = np.array(np.where(a > 3))
 print(b)
-# 使用np.transpose方法装置后，结果就能看懂了
+# 使用np.transpose方法装置后
 print(np.transpose(b))
 
 # 创建一个[3, 5] 所有元素为True的数组
